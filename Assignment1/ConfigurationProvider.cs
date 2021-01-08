49f87778591b9d8c46bfc3adbc224956b757988b
Brodey Yendall <S3718834@student.rmit.edu.au>
2021-01-08 23:19:09 +1100

    Added configuration to the project

---
+++ Assignment1/ConfigurationProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace Assignment1
+{
+    class ConfigurationProvider
+    {
+        private static readonly string configurationSourceFile = "appsettings.json";
+
+        private static IConfigurationRoot configurationRoot;
+
+        private const string databaseConnectionStringKey = "ConnectionString";
+        private const string dataSeedApiUrlKey = "DataSeedApiUrl";
+
+        public static String GetDatabaseConnectionString()
+        {
+            InitializeConfig();
+            return configurationRoot[databaseConnectionStringKey];
+        }
+
+        public static String GetDataSeedApiUrl()
+        {
+            InitializeConfig();
+            return configurationRoot[dataSeedApiUrlKey];
+        }
+
+        private static void InitializeConfig()
+        {
+            if(configurationRoot == null)
+            {
+                configurationRoot = new ConfigurationBuilder().AddJsonFile(configurationSourceFile).Build();
+            }
+        }
+    }
+}

43d8ddcdfdcb56aec3e12a56b01d355d12ce29d6
Brodey Yendall <S3718834@student.rmit.edu.au>
2021-01-09 19:16:23 +1100

    ConfigurationProvider now allows multiple configuration source files

--- Assignment1/ConfigurationProvider.cs
+++ Assignment1/ConfigurationProvider.cs
@@ -1,39 +1,52 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 
 namespace Assignment1
 {
     public static class ConfigurationProvider
     {
-        private const string ConfigurationSourceFile = "appsettings.json";
+        private const string DefaultConfigurationSourceFile = "appsettings.json";
 
-        private static IConfigurationRoot _configurationRoot;
+        private static readonly Dictionary<string, IConfigurationRoot> ConfigurationRoots =
+            new Dictionary<string, IConfigurationRoot>();
 
         private const string DatabaseConnectionStringKey = "ConnectionString";
         private const string CustomerDataSeedApiUrlKey = "CustomerDataSeedApiUrl";
         private const string LoginDataSeedApiUrlKey = "LoginDataSeedApiUrl";
 
 
-        public static string GetDatabaseConnectionString()
+        public static string GetDatabaseConnectionString(
+            string configurationSourceFile = DefaultConfigurationSourceFile)
         {
             InitializeConfig();
-            return _configurationRoot[DatabaseConnectionStringKey];
+            return ConfigurationRoots[configurationSourceFile][DatabaseConnectionStringKey];
         }
 
-        public static string GetCustomerDataSeedApiUrl()
+        public static string GetCustomerDataSeedApiUrl(string configurationSourceFile = DefaultConfigurationSourceFile)
         {
             InitializeConfig();
-            return _configurationRoot[CustomerDataSeedApiUrlKey];
-        }
-        
-        public static string GetLoginDataSeedApiUrl()
-        {
-            InitializeConfig();
-            return _configurationRoot[LoginDataSeedApiUrlKey];
+            return ConfigurationRoots[configurationSourceFile][CustomerDataSeedApiUrlKey];
         }
 
-        private static void InitializeConfig()
+        public static string GetLoginDataSeedApiUrl(string configurationSourceFile = DefaultConfigurationSourceFile)
         {
-            _configurationRoot ??= new ConfigurationBuilder().AddJsonFile(ConfigurationSourceFile).Build();
+            InitializeConfig();
+            return ConfigurationRoots[configurationSourceFile][LoginDataSeedApiUrlKey];
+        }
+
+        public static string GetConfig(string configKey,
+            string configurationSourceFile = DefaultConfigurationSourceFile)
+        {
+            return ConfigurationRoots[configurationSourceFile][configKey];
+        }
+
+        private static void InitializeConfig(string configurationSourceFile = DefaultConfigurationSourceFile)
+        {
+            if (!ConfigurationRoots.ContainsKey(configurationSourceFile))
+            {
+                ConfigurationRoots.Add(configurationSourceFile,
+                    new ConfigurationBuilder().AddJsonFile(DefaultConfigurationSourceFile).Build());
+            }
         }
     }
 }

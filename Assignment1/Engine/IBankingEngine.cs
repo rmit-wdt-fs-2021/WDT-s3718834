f2e1b2212335df9c9e75a5df8324d7348bb6632c
Brodey Yendall <S3718834@student.rmit.edu.au>
2021-01-10 15:36:03 +1100

    Update engine to have "make transfer" method async

--- Assignment1/Engine/IBankingEngine.cs
+++ Assignment1/Engine/IBankingEngine.cs
@@ -16,7 +16,7 @@ namespace Assignment1.Engine
         public Task<List<Account>> GetAccounts(Customer customer);
         public Task<List<Transaction>> GetTransactions(Account account);
 
-        public bool MakeTransfer(Account sourceAccount, Account destinationAccount, decimal amount);
+        public Task<bool> MakeTransfer(Account sourceAccount, Account destinationAccount, decimal amount);
 
         public Task<(bool wasSuccess, decimal endingBalance)> MakeTransaction(Account account, TransactionType transactionType, decimal amount);
 
